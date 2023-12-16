f0f58ae561cc0ece96b0c00f32f56b55253a21f6
SAURABH2711-DAR <83362966+SAURABH2711-DAR@users.noreply.github.com>
2023-12-16 10:54:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-// document.addEventListener('DOMContentLoaded', async () => {
-//     try {
-//         const videoId = 'dQw4w9WgXcQ';
-//         const YOUR_API_KEY = 'AIzaSyDXvphxAMVuOqXUlhwmA7I656sFcjSOQq4'; 
-//         // const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${YOUR_API_KEY}&part=snippet`;
-//         const apiUrl = `https://www.youtube.com/watch?v=${videoId}&t=15s`   
-
-//         const response = await fetch(apiUrl);
-//         const data = await response.json();
-
-//         const videoTitle = data.items[0].snippet.title;
-//         document.title = videoTitle; 
-
-//         const videoContainer = document.getElementById('video-container');
-//         videoContainer.innerHTML = `<iframe width="100%" height="100%" src="    " frameborder="0" allowfullscreen></iframe>`;
-//     } catch (error) {
-//         console.error('Error:', error);
-//         document.getElementById('video-container').innerHTML = '<h2>Error loading the video</h2>';
-//     }
-// });
-
 const videoId = "dQw4w9WgXcQ"; // Replace with your actual video ID
 
 function displayVideo() {
